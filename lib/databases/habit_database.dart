a349135da8ed17f678c52f92ce805ec3c43bacf5
Delan Gono <30848798+SteiKnight@users.noreply.github.com>
2024-12-30 10:17:02 +0100

    modified: added all the crud funtions

--- lib/databases/habit_database.dart
+++ lib/databases/habit_database.dart
@@ -1,10 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:habittracker/models/app_settings.dart';
 import 'package:habittracker/models/habit.dart';
 import 'package:isar/isar.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:path_provider/path_provider.dart';
 
-class HabitDatabase {
+class HabitDatabase extends ChangeNotifier {
   static late Isar isar;
 
   /// S E T U P
@@ -39,9 +39,89 @@ class HabitDatabase {
    */
 
   //list of habits
+  List<Habit> currentHabits = [];
+
   //create habit
-  //read habit
+  Future<void> addHabit(String habitName) async {
+    final newHabit = Habit()..name = habitName;
+
+    //save to db
+    await isar.writeTxn(
+      () => isar.habits.put(newHabit),
+    );
+
+    //fetch from db
+    readHabits();
+  }
+
+  //read habits from db
+  Future<void> readHabits() async {
+    //fetch all habits from db
+    List<Habit> newHabits = await isar.habits.where().findAll();
+
+    //give to current habits
+    currentHabits.clear();
+    currentHabits.addAll(newHabits);
+
+    //update ui
+    notifyListeners();
+  }
+
   //update habit on and off
+  Future<void> updateHabitCompletion(int id, bool isCompleted) async {
+    //find the specific Habit
+    final habit = await isar.habits.get(id);
+    final today = DateTime.now();
+    //update completion status
+    if (habit != null) {
+      await isar.writeTxn(() async {
+        //if habit is completed we add the current date to the database
+        if (isCompleted && !habit.completedDays.contains(DateTime.now())) {
+          final today = DateTime.now();
+          habit.completedDays.add(DateTime(
+            today.year,
+            today.month,
+            today.day,
+          ));
+        } else if (!isCompleted &&
+            habit.completedDays.contains(DateTime.now())) {
+          habit.completedDays.removeWhere((date) =>
+              date.year == today.year &&
+              date.month == today.year &&
+              date.day == today.year);
+        }
+        //save the updated habits
+        await isar.habits.put(habit);
+      });
+    }
+    //re-read from db
+    readHabits();
+  }
+
   //update habit name
+  Future<void> updateHabitName(int id, String name) async {
+    final habit = await isar.habits.get(id);
+
+    if (habit != null) {
+      habit.name = name;
+      await isar.writeTxn(() async {
+        await isar.habits.put(habit);
+      });
+    }
+
+    readHabits();
+  }
+
   //delete habit
+  Future<void> deleteHabit(int id) async {
+    final habit = await isar.habits.get(id);
+
+    if (habit != null) {
+      await isar.writeTxn(
+        () => isar.habits.delete(id),
+      );
+    }
+
+    readHabits();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:habittracker/databases/habit_database.dart';
 import 'package:habittracker/pages/home_page.dart';
 import 'package:habittracker/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //initialize database
+  await HabitDatabase.initialize();
+  await HabitDatabase().saveFirstLaunchDate();
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(
           create: (context) => ThemeProvider(),
-        )
+        ),
+        ChangeNotifierProvider(
+          create: (context) => HabitDatabase(),
+        ),
       ],
       child: MyApp(),
     ),
